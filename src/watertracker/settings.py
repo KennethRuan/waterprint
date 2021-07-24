cdd4a59658a546054a00987217955641e1a44181
Justin Lu <wabasabi359@gmail.com>
2021-07-24 14:25:28 -0400

    Change linking to Django style, scale elements in register, change styling

--- src/watertracker/settings.py
+++ src/watertracker/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'src/frontend/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
