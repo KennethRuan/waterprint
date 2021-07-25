19822af66f49a6a517eb920f7c00e091e51ae539
astrocat879 <astrocat879@gmail.com>
2021-07-25 02:22:52 -0400

    add search

--- src/frontend/water_usage.py
+++ src/frontend/water_usage.py
@@ -6,20 +6,34 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Profile
 from datetime import date, datetime, timedelta
-
+from watertracker.wsgi import *
 from .forms import CreateUserForm, WaterUsageForm, AddFriendsForm
+
+from nltk.tokenize import TreebankWordTokenizer
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
 
 @login_required(login_url="login")
 def water_usage_view(request):
     person = Profile.objects.filter(person_of=request.user).last()
     form = WaterUsageForm(instance=person)
-
+    arr = []
     if request.method == "POST":
-        form = WaterUsageForm(request.POST, instance=person)
-        if form.is_valid():
-            profile = form.save()
-            profile.save()
-            return redirect("home")
+        query = request.POST["searched"]
+        query_vector = vectorizer.transform([query]).todense()
+        similarity = cosine_similarity(query_vector, crop_vectors)
+        ranks = (-similarity).argsort(axis=None)
 
-    context = {"form":form}
-    return render(request,"frontend/water_usage.html", context)
+        return render(request,"water_usage.html", {"searched": ranks[0]})
+    return render(request,"water_usage.html", {})
+
+        # form = WaterUsageForm(request.POST, instance=person)
+        # if form.is_valid():
+        #     profile = form.save()
+        #     profile.save()
+        #     return redirect("home")
+
+    # context = {"form":form}
+    
--- src/watertracker/wsgi.py
+++ src/watertracker/wsgi.py
@@ -8,10 +8,44 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
-
 from dotenv import load_dotenv
+from nltk.tokenize import TreebankWordTokenizer
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
+import string
+
+REMOVE_PUNCTUATION_TABLE = str.maketrans({x: None for x in string.punctuation})
+TOKENIZER = TreebankWordTokenizer()
+STEMMER = PorterStemmer()
+
+
+def tokenize_and_stem(s):
+    return [STEMMER.stem(token) for token
+            in TOKENIZER.tokenize(s.translate(REMOVE_PUNCTUATION_TABLE))]
+
+
+data = pd.read_csv(r'crops.csv')
+df = pd.DataFrame(data)
+crops = df["Product description (HS)"].to_list()
+avg = df["Global average"].to_list()
+mp = {}
+crops2 = []
+
+for i in range(1, 1060, 3):
+    tmp = crops[i].split(",")
+    tmp = tmp[0]
+    tmp = tmp.split("(")
+    tmp = tmp[0]
+    crops2.append(tmp)
+    mp[tmp] = avg[i]+avg[i+1]+avg[i+2]
+
+vectorizer = TfidfVectorizer(tokenizer=tokenize_and_stem, stop_words='english')
+vectorizer.fit(crops2)
+crop_vectors = vectorizer.transform(crops2)
+
 
 project_folder = os.path.expanduser('~/waterprint')
 load_dotenv(os.path.join(project_folder, '.env'))
