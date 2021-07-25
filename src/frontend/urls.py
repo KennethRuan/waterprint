16c7c9ec8f200edc1a347f3f7afaec535b8d49d2
astrocat879 <astrocat879@gmail.com>
2021-07-25 06:27:31 -0400

    add all friends page

--- src/frontend/urls.py
+++ src/frontend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from .views import *
 from .water_usage import *
+from .friends import *
 
 urlpatterns = [
     path('', login_view),
@@ -10,4 +11,5 @@ urlpatterns = [
     path('register/', register_view, name="register"),
     path('water-usage/', water_usage_view, name="water-usage"),
     path('home/', home_view, name="home"),
+    path('friends/', friends_view, name="friends"),
 ]
